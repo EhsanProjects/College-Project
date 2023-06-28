1fd63671559a515c903dd6149da5777eb5b5a8f8
unknown <ehsan.heidarian2020@gmail.com>
2023-06-27 20:37:47 -0500

    Help page Added

--- blueprints/general.py
+++ blueprints/general.py
@@ -3,7 +3,10 @@ from flask import Blueprint
 app = Blueprint("general", __name__)
 
 @app.route('/')
-def hello_world():  # put application's code here
+def main():  # put application's code here
     return 'This is main page!'
 
 
+@app.route('/help')
+def help():  # put application's code here
+    return 'Help page!'
